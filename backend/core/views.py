1bf9626cc218a59cf30500731508e005043f2aef
kavyapurohit <156873691+kavyapurohit@users.noreply.github.com>
2025-03-16 10:21:44 +0530

    views.py

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse, HttpResponseRedirect
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow
